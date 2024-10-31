dffa558ed031531de3a53440a0428d2d57b6b426
Ryota Sugawara <sugawararyota0813@icloud.com>
2024-10-31 13:07:07 +0900

    test

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 
 ng () {
 	echo ${1}行目が違うよ
@@ -7,8 +7,19 @@ ng () {
 
 res=0
 
-out=$(seq 5 | ./plus_stdin)
-[ "${out}" = 15 ] || ng "$LINENO"
+out=$(seq 5 | ./plus)
+++ seq 5
+++ ./plus
++ ./out=15
 
-[ "${res}" = 0] && echo OK
+###正常な場合###
+[ "${out}" = 15 ] || ng "$LINENO"
++ '[' 15 = 15 ']'
+
+###異常な場合###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+[ "${res}" = 0 ] && echo OK
 exit $res
